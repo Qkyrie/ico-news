3676f7e27ff19253f72f57bd9145da4ba2c724dc
Quinten De Swaef <quintendeswaef@gmail.com>
2016-12-07 22:38:44 +0100

    main class and builds now

---
+++ web/src/main/java/io/iconews/web/WebApplication.java
@@ -0,0 +1,14 @@
+package io.iconews.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(WebApplication.class);
+        application.run(args);
+    }
+
+}
