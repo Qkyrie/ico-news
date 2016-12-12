f1fedbf73df5a70bb1a55ac0fb62b6cafdb7f842
qdeswaef <quinten.de.swaef@persgroep.be>
2016-12-12 16:29:05 +0100

    moving to static filepath in resources

--- web/src/main/java/io/iconews/web/controller/IndexController.java
+++ web/src/main/java/io/iconews/web/controller/IndexController.java
@@ -2,7 +2,6 @@ package io.iconews.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
